e9e640e577a457072e678ccaaedf4c91ea6fb00b
Ilyavosky <ilyacortesruiz@gmail.com>
2026-04-18 00:35:26 -0600

    Mejorar la validación de schemas

--- src/lib/validations/schema.ts
+++ src/lib/validations/schema.ts
@@ -1,83 +1,76 @@
 import { z } from "zod";
 
-//Coerce: Funcionalidad que convierte automáticamente los datos de entrada a un tipo específico antes de validar el esquema
-
 export const idSchema = z.coerce
-  .number({ error : 'El id debe ser un número'})
+  .number({ error: 'El id debe ser un número' })
   .int('El ID no puede tener decimales')
   .positive('El ID debe ser mayor a 0')
   .min(1, 'El ID es requerido');
 
 export const spotifyIdSchema = z.string()
-    .min(1, 'El Spotify_artist id es requerido')
-    .max(100, 'El Spotify_artist id no puede exceder los 100 caracteres');
+  .min(1, 'El Spotify id es requerido')
+  .max(100, 'El Spotify id no puede exceder los 100 caracteres');
 
 export const genresSchema = z.object({
-  slug: z.string().min(1, 'El slug es requerido').max(100, 'El slug no puede exceder los 100 caracteres'),
   id: idSchema,
-  name: z.string().min(1, 'El nombre es requerido').max(100, 'El nombre no puede exceder los 100 caracteres')
+  slug: z.string().min(1, 'El slug es requerido').max(100, 'El slug no puede exceder los 100 caracteres'),
+  name: z.string().min(1, 'El nombre es requerido').max(100, 'El nombre no puede exceder los 100 caracteres'),
 });
 
 export const generationSessionsSchema = z.object({
-    id : idSchema,
-    session_uuid: z.string().uuid(),
-    name : z.string().min(1, 'El nombre es requerido')
-    .max(255, 'El nombre no debe exceder los 255 caracteres'),
-    target_valence: z.number().nonnegative(),
-    target_energy: z.number().nonnegative(),
-    target_danceability: z.number().nonnegative(),
-    is_exporte: z.boolean().default(false),
-    created_at: z.coerce.date().default(() => new Date()),
-    updated_at: z.coerce.date().default(() => new Date())
+  id: idSchema,
+  session_uuid: z.string().uuid(),
+  name: z.string().min(1, 'El nombre es requerido').max(255, 'El nombre no debe exceder los 255 caracteres'),
+  target_valence: z.number().nonnegative(),
+  target_energy: z.number().nonnegative(),
+  target_danceability: z.number().nonnegative(),
+  is_exported: z.boolean().default(false),
+  created_at: z.coerce.date().default(() => new Date()),
+  updated_at: z.coerce.date().default(() => new Date()),
 });
 
 export const sessionSeedGenresSchema = z.object({
-    session_id: idSchema,
-    genre_id: idSchema  
+  session_id: idSchema,
+  genre_id: idSchema,
 });
 
 export const artistSchema = z.object({
-    id: idSchema,
-    spotify_id: spotifyIdSchema,
-    name: z.string().min(100, 'El nombre del artista es neceario')
-    .max(255, 'El nombre del artista no debe excedere los 255 caracteres'),
-    img_url: z.string().url('La URL de la imagen debe ser valida')
+  id: idSchema,
+  spotify_id: spotifyIdSchema,
+  name: z.string().min(1, 'El nombre del artista es requerido').max(255, 'El nombre del artista no debe exceder los 255 caracteres'),
+  image_url: z.string().url('La URL de la imagen debe ser válida').optional(),
 });
 
 export const albumsSchema = z.object({
-    id: idSchema,
-    spotify_id: spotifyIdSchema,
-    name: z.string().min(100, 'El nombre del artista es neceario')
-    .max(255, 'El nombre del album no debe excedere los 255 caracteres'),
-    release_date: z.coerce.date().default(() => new Date()),
-    cover_url: z.string().url('La URL de la imagen debe ser valida') 
+  id: idSchema,
+  spotify_id: spotifyIdSchema,
+  name: z.string().min(1, 'El nombre del álbum es requerido').max(255, 'El nombre del álbum no debe exceder los 255 caracteres'),
+  release_date: z.coerce.date().optional(),
+  cover_url: z.string().url('La URL de la imagen debe ser válida').optional(),
 });
 
 export const tracksSchema = z.object({
-    id : idSchema,
-    spotifyId: spotifyIdSchema,
-    name: z.string().min(100, 'El nombre del artista es neceario')
-    .max(255, 'El nombre de las canciones no debe excedere los 255 caracteres'),
-    duration_ms: z.number().nonnegative('La duracion debe ser mayor a 0 ms'),
-    url: z.string().url('La URL del track debe ser valida'),
-    album_id: idSchema,
-    valence: z.number().nonnegative(),
-    energy: z.number().nonnegative(),
-    danceability: z.number().nonnegative(),
+  id: idSchema,
+  spotify_id: spotifyIdSchema,
+  name: z.string().min(1, 'El nombre del track es requerido').max(255, 'El nombre del track no debe exceder los 255 caracteres'),
+  duration_ms: z.number().nonnegative('La duración debe ser mayor a 0 ms').optional(),
+  uri: z.string().min(1, 'El URI del track es requerido'),
+  album_id: idSchema.optional(),
+  valence: z.number().nonnegative().optional(),
+  energy: z.number().nonnegative().optional(),
+  danceability: z.number().nonnegative().optional(),
 });
 
 export const trackArtistsSchema = z.object({
-    track_id: idSchema,
-    artist_id: idSchema,
-    is_main_artist: z.boolean().default(false),
+  track_id: idSchema,
+  artist_id: idSchema,
+  is_main_artist: z.boolean().default(false),
 });
 
 export const sessionTrackSchema = z.object({
-    id: idSchema,
-    session_id: idSchema,
-    track_id: idSchema,
-    track_status: z.enum(['pending', 'accepted', 'rejected'])
-    .default('pending'),
-    added_at: z.coerce.date().default(() => new Date()),
-    updated_at: z.coerce.date().default(() => new Date())
+  id: idSchema,
+  session_id: idSchema,
+  track_id: idSchema,
+  status: z.enum(['pending', 'accepted', 'rejected']).default('pending'),
+  added_at: z.coerce.date().default(() => new Date()),
+  updated_at: z.coerce.date().default(() => new Date()),
 });
