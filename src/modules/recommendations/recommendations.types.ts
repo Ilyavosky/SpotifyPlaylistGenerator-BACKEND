cddc9073fe8b4a3bbad51f3f8c0a6068e7274e29
Ilyavosky <ilyacortesruiz@gmail.com>
2026-04-18 03:25:35 -0600

    Implementar tipado para las recomendaciones del algoritmo

---
+++ src/modules/recommendations/recommendations.types.ts
@@ -0,0 +1,39 @@
+export interface RecommendationParams {
+  target_valence: number;
+  target_energy: number;
+  target_danceability: number;
+  seed_genres: string[];
+}
+
+export interface SpotifyTrack {
+  id: string;
+  name: string;
+  uri: string;
+  duration_ms: number;
+  album: {
+    id: string;
+    name: string;
+    release_date: string;
+    images: { url: string }[];
+  };
+  artists: {
+    id: string;
+    name: string;
+    images?: { url: string }[];
+  }[];
+}
+
+export interface SpotifyRecommendationsResponse {
+  tracks: SpotifyTrack[];
+}
+
+export interface SavedTrack {
+  id: number;
+  spotify_id: string;
+  name: string;
+  uri: string;
+  duration_ms: number;
+  valence: number;
+  energy: number;
+  danceability: number;
+}
