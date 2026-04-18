179ccb674626a1d4ecfd6225969030f0ce73bdd3
Ilyavosky <ilyacortesruiz@gmail.com>
2026-04-18 02:34:26 -0600

    Implementar manejo de obtención de generos

---
+++ src/modules/genres/genres.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import * as genresService from './genres.service';
+import { AppError } from '../../lib/errors/AppError';
+
+export async function getGenres(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const accessToken = req.cookies?.access_token;
+
+    if (!accessToken) {
+      throw new AppError('UNAUTHORIZED', 'No access token', 401);
+    }
+
+    const genres = await genresService.getGenresFromSpotify(accessToken);
+    await genresService.syncGenres(genres);
+
+    res.json({ genres });
+  } catch (err) {
+    next(err);
+  }
+}
