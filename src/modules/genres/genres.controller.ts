1462e74b5a9ae2e97fd1002ded31477c4bd40ed3
Ilyavosky <ilyacortesruiz@gmail.com>
2026-04-19 23:17:35 -0600

    Corregir controller

--- src/modules/genres/genres.controller.ts
+++ src/modules/genres/genres.controller.ts
@@ -1,25 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 import * as genresService from './genres.service';
-import { AppError } from '../../lib/errors/AppError';
 
-export async function getGenres(req: Request, res: Response, next: NextFunction): Promise<void> {
+export async function getGenres(_req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
-    const cached = await genresService.getGenresFromDb();
-
-    if (cached.length > 0) {
-      res.json({ genres: cached.map(g => g.slug) });
-      return;
-    }
-
-    const accessToken = req.cookies?.access_token;
-
-    if (!accessToken) {
-      throw new AppError('UNAUTHORIZED', 'No access token', 401);
-    }
-
-    const genres = await genresService.getGenresFromSpotify(accessToken);
-    await genresService.syncGenres(genres);
-
+    const genres = await genresService.getGenresFromSpotify('');
     res.json({ genres });
   } catch (err) {
     next(err);
